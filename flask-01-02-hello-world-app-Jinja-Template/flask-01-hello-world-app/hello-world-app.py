8a8f513ccf644df431ad64dc18017051479d879b
Tugce <boyno.8823@gmail.com>
2021-06-15 21:45:44 -0400

    our application

--- flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -20,4 +20,5 @@ def forth(id):
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', post=80)
---
+++ flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+    return render_template('index.html', number1=12, number2=15)
+
+@app.route('/sum')
+def sum():
+    num1 , num2 = 12, 15
+    return render_template('body.html', value=num1, value2=num2, value3=num1+num2)
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
